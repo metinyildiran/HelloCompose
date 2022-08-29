39c2abed6928c00776035318d14c111164275516
Metin Yıldıran <metin.1464@gmail.com>
2022-08-29 17:42:39 +0300

    Added a few Composable

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -3,41 +3,65 @@ package com.example.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.hellocompose.ui.theme.HelloComposeTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CustomText(text = "Hello!")
+        Spacer(modifier = Modifier.padding(5.dp))
+        CustomText(text = "Howdy!")
+        Spacer(modifier = Modifier.padding(5.dp))
+        CustomText(text = "Hi!")
+        Spacer(modifier = Modifier.padding(5.dp))
+        CustomText(text = "Wassup!")
+        Spacer(modifier = Modifier.padding(5.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            CustomText(text = "I'm fine!")
+            CustomText(text = "You?")
+        }
+    }
+}
+
+@Composable
+fun CustomText(text: String) {
+    Text(
+        modifier = Modifier
+            .background(MaterialTheme.colors.secondary)
+            .padding(10.dp),
+        //.fillMaxWidth(),
+        text = text,
+        color = MaterialTheme.colors.primary,
+        fontSize = 25.sp,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    HelloComposeTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
